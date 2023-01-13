e7a90eb02ee8307a327e07744f588e0c2f6694a2
ujjaval-shah <uss1999int@gmail.com>
2023-01-13 17:46:34 +0530

    classes are now puclic.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,14 +2,14 @@ package com.driver;
 
 public class Main {
 
-    static class A {
-        String meth() {
+    public static class A {
+        public String meth() {
             return "Invoking method from class A";
         }
     }
 
-    static class B extends A {
-        String meth() {
+    public static class B extends A {
+        public String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
